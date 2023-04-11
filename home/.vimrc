ec460f768b5fb9b4a604d433fdcfd1e5fea81aff
mike-a2z <mikepan@amazon.com>
2023-04-10 19:54:26 -0700

    feat: home config

---
+++ home/.vimrc
@@ -0,0 +1,20 @@
+set history=10000
+set laststatus=2
+set number
+set ruler
+
+syntax on
+filetype on
+
+set autoindent
+set cindent
+set smartindent
+set tabstop=2
+set softtabstop=2
+set expandtab
+set smarttab
+
+set showmatch
+set nohls
+set hlsearch
+set incsearch
---
+++ home/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo === Script directory: $SCRIPT_DIR
+
+function link() {
+    link_src=$1
+    link_dest=$2
+    echo === Linking $link_dest with $link_src
+    if [ -L "$link_dest" ]; then rm "$link_dest"; fi
+    if [ -f "$link_dest" ]; then mv "$link_dest" "$link_dest.bak"; fi
+    ln -sf "$link_src" "$link_dest"
+    ls -al "$link_dest"
+}
+
+# ~/.vimrc
+link $SCRIPT_DIR/.vimrc ~/.vimrc
+
+# vscode settings.json
+VSCODE_JSON=vscode.json
+VSCODE_SETTINGS=User/settings.json
+for VSCODE in "/Users/$USER/Library/Application Support/Code" "/Users/$USER/Library/Application Support/Code - Insiders" "/Users/$USER/Library/Application Support/VSCodium"
+do
+    if [ -L "$VSCODE/$VSCODE_SETTINGS" ]; then rm "$VSCODE/$VSCODE_SETTINGS"; fi
+    if [ -f "$VSCODE/$VSCODE_SETTINGS" ]; then mv "$VSCODE/$VSCODE_SETTINGS" "$VSCODE/$VSCODE_SETTINGS.bak"; fi
+    ln -sf "$SCRIPT_DIR/$VSCODE_JSON" "$VSCODE/$VSCODE_SETTINGS"
+    ls -al "$VSCODE/$VSCODE_SETTINGS"
+done
